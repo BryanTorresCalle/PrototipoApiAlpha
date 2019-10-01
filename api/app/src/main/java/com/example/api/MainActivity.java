fae49fe54a695e2a6dbd602794b68a29ce5d7a6a
Bryan_Torres_Calle <bryk347@gmail.com>
2019-10-01 09:18:16 -0500

    Se integra app con firebae

--- api/app/src/main/java/com/example/api/MainActivity.java
+++ api/app/src/main/java/com/example/api/MainActivity.java
@@ -8,15 +8,21 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     Animation from, fromTop;
     ImageView logo;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
 
         from = AnimationUtils.loadAnimation(this, R.anim.from);
