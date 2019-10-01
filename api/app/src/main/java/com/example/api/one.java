63c99310f601c1e1aaf116cdddb4c376d468d99b
Bryan_Torres_Calle <bryk347@gmail.com>
2019-10-01 09:50:21 -0500

    Se agrega la programación del botón de omitir

--- api/app/src/main/java/com/example/api/one.java
+++ api/app/src/main/java/com/example/api/one.java
@@ -1,13 +1,24 @@
 package com.example.api;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class one extends AppCompatActivity {
+
+    Button btnSkip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        btnSkip = findViewById(R.id.btnSkip);
+
+        btnSkip.setOnClickListener( o ->  startActivity(new Intent(getApplicationContext(), autentication.class)));
+
+
+
     }
 }
